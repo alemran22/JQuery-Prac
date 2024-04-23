d286af537898beb01122d62f6dbfab458cb81986
alemran22 <alemrangazi@gmail.com>
2024-04-23 21:18:21 +0600

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    //    write text
+    
+    // $(".button").click(function () {
+    //     $("h1").text("I am hidden");
+    // }
+    // ); 
+  
+    // hide text
+
+    //    $(".button").click(function () {
+    //     $("h1").hide("I am hidden");
+    // }
+    // ); 
+
+// change html
+    
+    //    $(".button").click(function () {
+    //     $("h1").html("<p>I am a paragraph.</p>");
+    // }
+    // ); 
+    
+    // hide this
+    
+    //     $(".button").click(function () {
+    //         $(this).hide();
+    // }
+    // ); 
+    
+    // css
+    
+        $(".button").click(function () {
+            $("h1").css({"font-size": "40px", "font-weight": "bold", "margin": "20px"});
+    }
+    ); 
+});
